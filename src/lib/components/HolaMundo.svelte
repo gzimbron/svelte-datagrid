3ca59c1a2411694700b3b1adc71dfcd82cf74c8b
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-08 14:24:52 -0600

    flow test

---
+++ src/lib/components/HolaMundo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<h2>Hola Mundo</h2>
+
+<style lang="postcss">
+	h2 {
+		color: red;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import HolaMundo from '$lib/components/HolaMundo.svelte';
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<HolaMundo></HolaMundo>
