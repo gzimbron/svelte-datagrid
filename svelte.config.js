e97beb35f16a07c2dab24fc16980000ab060a473
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-08 14:54:32 -0600

    Actualizar configuración de Svelte y adaptador

--- svelte.config.js
+++ svelte.config.js
@@ -5,15 +5,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? undefined : process.env.BASE_PATH
 		},
 		alias: {
 			$siteconfig: './src/siteconfig.ts',
