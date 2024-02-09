90c2442b08c810a0879506a69a3c4c8721b7a79a
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-08 18:35:32 -0600

    fix lint

--- src/siteconfig.d.ts
+++ src/siteconfig.d.ts
@@ -1,9 +1,9 @@
-export declare const SITE_NAME = "Svelte DataGrid";
-export declare const SITE_DESCRIPTION = "A high-performance Svelte DataGrid component";
-export declare const SITE_URL = "https://gzimbron.github.io/svelte-datagrid";
-export declare const SITE_IMAGE = "https://gzimbron.github.io/svelte-datagrid/og-image.webp";
-export declare const SITE_GITHUB_URL = "https://github.com/gzimbron/svelte-datagrid";
+export declare const SITE_NAME = 'Svelte DataGrid';
+export declare const SITE_DESCRIPTION = 'A high-performance Svelte DataGrid component';
+export declare const SITE_URL = 'https://gzimbron.github.io/svelte-datagrid';
+export declare const SITE_IMAGE = 'https://gzimbron.github.io/svelte-datagrid/og-image.webp';
+export declare const SITE_GITHUB_URL = 'https://github.com/gzimbron/svelte-datagrid';
 export declare const MENU_LINKS: {
-    name: string;
-    url: string;
+	name: string;
+	url: string;
 }[];
