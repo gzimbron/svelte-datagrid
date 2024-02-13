625bee61e3a4b9cb805de88e3d8f0d3918314841
Gustavo Zimbrón <pptavozl@gmail.com>
2024-02-12 19:11:41 -0600

    refactor(Website): Fix lint

--- apps/website/src/routes/+page.svelte
+++ apps/website/src/routes/+page.svelte
@@ -99,6 +99,7 @@
 		}
 	};
 
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	let getGridState: () => any | undefined;
 	let scrollToRow: (index: number) => void | undefined;
 </script>
